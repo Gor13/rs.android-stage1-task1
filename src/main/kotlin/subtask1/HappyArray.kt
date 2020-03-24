f98b6a27608c6a827abda8dab6118131f1273b26
Hardzei <pavelko1331@gmail.com>
2020-03-24 11:34:26 +0300

    feat: task1 completed

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,50 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var sadArray = sadArray
+        val happyArray = mutableListOf<Int>()
+        if (sadArray.size != 0){
+
+            happyArray.add(sadArray[0])
+            for (i in 1..sadArray.size - 2){
+                if (sadArray[i] < (sadArray[i-1]+ sadArray[i+1])){
+                    happyArray.add(sadArray[i])
+                }
+
+            }
+            happyArray.add(sadArray[sadArray.size-1])
+
+            sadArray = happyArray.toIntArray()
+            happyArray.clear()
+
+
+            happyArray.add(sadArray[0])
+            for (i in 1..sadArray.size - 2){
+                if (sadArray[i] < (sadArray[i-1]+ sadArray[i+1])){
+                    happyArray.add(sadArray[i])
+                }
+
+            }
+            happyArray.add(sadArray[sadArray.size-1])
+
+            sadArray = happyArray.toIntArray()
+            happyArray.clear()
+
+
+            happyArray.add(sadArray[0])
+            for (i in 1..sadArray.size - 2){
+                if (sadArray[i] < (sadArray[i-1]+ sadArray[i+1])){
+                    happyArray.add(sadArray[i])
+                }
+
+            }
+            happyArray.add(sadArray[sadArray.size-1])
+            for (i in happyArray){
+                println(i)
+            }
+            return happyArray.toIntArray()
+        }
+
+        return happyArray.toIntArray()
     }
 }
